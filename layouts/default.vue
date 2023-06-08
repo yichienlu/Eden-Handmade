70aa182d66da0c0d3737a85df1564656a6b53331
yichienlu <yichien.lu89@gmail.com>
2023-06-08 17:19:07 +0800

    feat: bootstrap.js plugin

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <script setup>
   import 'popper.js'
-  // import 'bootstrap/dist/js/bootstrap.min.js'
 </script>
 <template>
   <div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,29 +6,5 @@ export default defineNuxtConfig({
       PATH: process.env.VUE_APP_PATH
     }
   },
-  app:{
-    head:{
-      script:[
-        // {
-        //   src: "popper.js/dist/popper.min.js",
-        //   crossorigin: "anonymous"
-        // },
-        // {
-        //   src: "bootstrap/bootstrap.js",
-        //   crossorigin: "anonymous"
-        // },
-        // {
-        //   src: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
-        //   integrity:"sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r",
-        //   crossorigin: "anonymous"
-        // },
-        {
-          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js",
-          integrity:"sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS",
-          crossorigin: "anonymous"
-        }
-      ]
-    }
-  },
   css: ["@/assets/css/all.scss"],
 })
