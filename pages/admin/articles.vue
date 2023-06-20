ef5be5e46bd9b432cba824e8a29eb8e33239fc8b
yichienlu <yichien.lu89@gmail.com>
2023-06-20 17:55:22 +0800

    feat: [adminArticles] variables

--- pages/admin/articles.vue
+++ pages/admin/articles.vue
@@ -3,8 +3,37 @@ definePageMeta({
   layout: 'admin'
 })
 
+const config = useRuntimeConfig();
+const articles = ref([])
+const tempArticle = ref({})
+const pagination = ref({})
+let currentPage = ref(1)
+let isLoading = ref(false)
 
+const getArticles = async (page = 1) => {
+  isLoading.value = true
+  currentPage.value = page
+  await useFetch(`${config.public.URL}/api/${config.public.PATH}/admin/articles/?page=${page}`,
+  {
+    headers: { Authorization: useCookie('hexToken').value },
+  })
+  .then((res)=>{
+    if(res.error.value){
+      isLoading.value = false
+      console.log(res.error.value.data.message)
+    } else {
+      isLoading.value = false
+      articles.value = res.data.value.articles
+      pagination.value = res.data.value.pagination
+      // console.log(res.data)
+      console.log(res.data.value.message)
+    }
+  })
 
+}
+onMounted(()=>{
+  getArticles()
+})
 </script>
 
 <template>
